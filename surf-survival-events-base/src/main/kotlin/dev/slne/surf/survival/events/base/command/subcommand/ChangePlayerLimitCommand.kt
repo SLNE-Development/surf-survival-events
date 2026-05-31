ef7a0207256b6f698cdafb9ec2d242242ed45ae3
MikeyLLP <167801877+MikeyLLP@users.noreply.github.com>
2026-05-31 12:54:15 +0200

    null

--- surf-survival-events-base/src/main/kotlin/dev/slne/surf/survival/events/base/command/subcommand/ChangePlayerLimitCommand.kt
+++ surf-survival-events-base/src/main/kotlin/dev/slne/surf/survival/events/base/command/subcommand/ChangePlayerLimitCommand.kt
@@ -10,7 +10,6 @@ import dev.slne.surf.survival.events.base.service.GameService
 import dev.slne.surf.survival.events.base.util.commands.PermissionRegistry
 
 fun CommandTree.changePlayerLimitCommand() = literalArgument("spielerlimit") {
-
     withPermission(PermissionRegistry.COMMAND_COMMUNITY_MANAGER)
     integerArgument("playerLimit") {
         playerExecutor { player, args ->
