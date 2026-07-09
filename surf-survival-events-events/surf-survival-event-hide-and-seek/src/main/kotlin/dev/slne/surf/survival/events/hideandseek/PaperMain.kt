7741f7781cedad95128ede18b1102d11bfa4ebe2
Timonso <management@timonso.de>
2026-07-09 20:33:02 +0200

    ✨ feat(hideandseek): preload event world for Hide and Seek game on enable

--- surf-survival-events-events/surf-survival-event-hide-and-seek/src/main/kotlin/dev/slne/surf/survival/events/hideandseek/PaperMain.kt
+++ surf-survival-events-events/surf-survival-event-hide-and-seek/src/main/kotlin/dev/slne/surf/survival/events/hideandseek/PaperMain.kt
@@ -1,12 +1,16 @@
 package dev.slne.surf.survival.events.hideandseek
 
 import com.github.shynixn.mccoroutine.folia.SuspendingJavaPlugin
+import com.github.shynixn.mccoroutine.folia.globalRegionDispatcher
+import com.github.shynixn.mccoroutine.folia.launch
 import dev.slne.surf.survival.events.base.game.GameRegistry
 import dev.slne.surf.survival.events.base.game.GameStopReason
+import dev.slne.surf.survival.events.base.game.GameWorldService
 import dev.slne.surf.survival.events.base.service.GameService
 import dev.slne.surf.survival.events.hideandseek.command.hideAndSeekCommand
 import dev.slne.surf.survival.events.hideandseek.config.HideAndSeekConfig
 import dev.slne.surf.survival.events.hideandseek.game.HideAndSeekGame
+import kotlinx.coroutines.CoroutineStart
 import org.bukkit.plugin.java.JavaPlugin
 
 val plugin get() = JavaPlugin.getPlugin(PaperMain::class.java)
@@ -19,7 +23,24 @@ class PaperMain : SuspendingJavaPlugin() {
     override suspend fun onEnableAsync() {
         hideAndSeekCommand()
 
-        GameRegistry.register(HideAndSeekGame.KEY, HideAndSeekGame())
+        val game = HideAndSeekGame()
+        GameRegistry.register(HideAndSeekGame.KEY, game)
+
+        preloadEventWorld(game)
+    }
+
+    private fun preloadEventWorld(game: HideAndSeekGame) {
+        launch(globalRegionDispatcher, CoroutineStart.DEFAULT) {
+            try {
+                GameWorldService.loadOrCreateEventWorld(
+                    HideAndSeekGame.KEY,
+                    game::customizeWorldCreator,
+                    game::customizeEventWorld
+                )
+            } catch (throwable: Throwable) {
+                componentLogger.error("Failed to preload the hide and seek event world", throwable)
+            }
+        }
     }
 
     override suspend fun onDisableAsync() {
